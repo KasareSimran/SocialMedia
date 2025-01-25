8b702f4941c7967333f863f93dc39727a0a9b11a
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-25 18:04:00 +0530

    social media app

---
+++ src/main/java/com/SocialMedia/SocialMedia/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.SocialMedia.SocialMedia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "this is home controller";
+    }
+}
