f3372a2b543283478cb8012beef3748136ef605b
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-01-31 15:18:42 +0530

    update db and post

--- src/main/java/com/SocialMedia/SocialMedia/config/AppConfig.java
+++ src/main/java/com/SocialMedia/SocialMedia/config/AppConfig.java
@@ -1,4 +1,22 @@
 package com.SocialMedia.SocialMedia.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+
+        http.authorizeHttpRequests(Authorize -> Authorize
+                .requestMatchers("api/**").authenticated()
+                .anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
 }
